bd39f2f131656464159c4b1cc3e9bd4c3478076b
nrymarz <nrymarz@gmail.com>
2020-12-03 17:28:07 -0600

    commit tree fe8c306b7c2d51f06f7b2c8fab40c8e5a9ea47d8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,39 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get "/recipes" do 
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/recipes' do 
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  patch '/recipes/:id' do 
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do 
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.destroy
+    redirect '/recipes'
+  end
 end
